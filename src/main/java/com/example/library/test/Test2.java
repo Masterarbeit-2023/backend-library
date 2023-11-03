72324cb58af8bf1450383d2efc7e7cfd2a7caa09
Niklas Röske <n.roeske@ostfalia.de>
2023-11-03 23:02:06 +0100

    add extra statements to Test2 class

--- src/main/java/com/example/library/test/Test2.java
+++ src/main/java/com/example/library/test/Test2.java
@@ -5,9 +5,15 @@ import com.example.library.annotation.parameter.HttpTrigger;
 
 
 public class Test2 {
+
+    Test test;
     @ApiFunction
     public void test(@HttpTrigger() Object object){
         System.out.println("Test test 123");
+        int i = 1+1;
+        for (; i<10; i++){
+            System.out.println(i);
+        }
     }
 
     @ApiFunction

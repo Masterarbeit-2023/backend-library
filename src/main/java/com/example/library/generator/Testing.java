718ceedaa60e90a34268dd1deda2fbcf7147667b
Niklas Röske <n.roeske@ostfalia.de>
2023-11-05 17:11:53 +0100

    add Testing.java to test library inside of another project

---
+++ src/main/java/com/example/library/generator/Testing.java
@@ -0,0 +1,22 @@
+package com.example.library.generator;
+
+import com.example.library.generator.config.Configuration;
+import com.example.library.generator.helper.ProjectDefinition;
+
+import static com.example.library.generator.YamlReader.readYaml;
+
+public class Testing {
+
+    public static Configuration configuration;
+    public static ProjectDefinition project;
+
+    public static void main(String[] args) {
+        test("", new Exception().getStackTrace()[0].getClassName());
+    }
+    public static void test(String projectRoot, String classToIgnored) {
+        configuration = readYaml("config.yaml");
+        System.out.println(configuration);
+        project = Reader.readProject(projectRoot, classToIgnored);
+        System.out.println(project);
+    }
+}

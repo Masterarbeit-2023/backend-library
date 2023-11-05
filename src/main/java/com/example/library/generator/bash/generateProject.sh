f7f79e4a51e66115404d65dec1b0204ba6bddf7c
Niklas Röske <n.roeske@ostfalia.de>
2023-11-05 17:08:46 +0100

    delete bash script

--- src/main/java/com/example/library/generator/bash/generateProject.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mvn archetype:generate -DgroupId=ToolsQA -DartifactId=$1 -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
-#echo "Hello from Bash script, argument: $1"

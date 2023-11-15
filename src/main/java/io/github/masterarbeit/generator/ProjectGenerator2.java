c6c42efba93be6a74d577b28059ef77e054b2b3f
Niklas Röske <n.roeske@ostfalia.de>
2023-11-15 18:43:40 +0100

    remove unnecessary methods out of ProjectGenerator2

--- src/main/java/io/github/masterarbeit/generator/ProjectGenerator2.java
+++ src/main/java/io/github/masterarbeit/generator/ProjectGenerator2.java
@@ -4,8 +4,6 @@ import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.stmt.Statement;
 import io.github.masterarbeit.Main;
 import io.github.masterarbeit.generator.config.Configuration;
-import io.github.masterarbeit.generator.config.Infrastructure;
-import io.github.masterarbeit.generator.config.ProviderEnum;
 import io.github.masterarbeit.generator.helper.*;
 import io.github.masterarbeit.generator.helper.method.*;
 import io.github.masterarbeit.util.HttpMethod;
@@ -14,96 +12,10 @@ import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class ProjectGenerator2 {
 
-    public static List<ProjectDeclaration> generateProjectDeclarationWithProjectConfig(ProjectDeclaration projectDeclaration, Configuration configuration) {
-        Infrastructure infrastructure = configuration.getInfrastructure();
-        ProviderEnum provider = configuration.getProvider();
-
-        switch (infrastructure) {
-            case TRADITIONAL -> {
-                return generateProjectDeclarationTraditional(projectDeclaration);
-            }
-            case SERVERLESS -> {
-                return generateProjectDeclarationServerless(projectDeclaration, provider);
-            }
-            case MICROSERVICES -> {
-                return generateProjectDeclarationMicroservice(projectDeclaration, provider);
-            }
-
-        }
-
-        return new ArrayList<>();
-    }
-
-    private static List<ProjectDeclaration> generateProjectDeclarationTraditional(ProjectDeclaration projectDeclaration) {
-        List<ProjectDeclaration> projectDeclarations = new ArrayList<>();
-        return projectDeclarations;
-    }
-
-    private static List<ProjectDeclaration> generateProjectDeclarationServerless(ProjectDeclaration projectDeclaration, ProviderEnum provider) {
-        List<ProjectDeclaration> projectDeclarations = new ArrayList<>();
-        for (ClassDeclaration classDeclaration : projectDeclaration.getClassDeclarations()) {
-            ProjectDeclaration newProjectDeclaration;
-            if (classDeclaration.containsApiFunctionAnnotation()) {
-                for (MethodDeclaration method : classDeclaration.getMethods()) {
-                    if (method.containsAnnotationApiFunction()) {
-                        newProjectDeclaration = new ProjectDeclaration();
-                        newProjectDeclaration.setName(method.getName());
-                        ClassDeclaration methodClassDeclaration = new ClassDeclaration();
-
-                        methodClassDeclaration.setName(method.getName());
-                        methodClassDeclaration.setPackageDeclaration("com.example");
-                        methodClassDeclaration.setFields(classDeclaration.getFields());
-
-                        MethodDeclaration methodToGenerateDeclaration = new MethodDeclaration();
-                        methodToGenerateDeclaration.setName(method.getName());
-                        methodToGenerateDeclaration.setBody(method.getBody());
-                        methodToGenerateDeclaration.setReturnType(method.getReturnType());
-                        methodToGenerateDeclaration.setAnnotations(processServerlessAnnotations(method.getName(), provider));
-
-                        methodToGenerateDeclaration.setParameters(method.getParameters());
-                        methodClassDeclaration.addMethod(methodToGenerateDeclaration);
-
-                        newProjectDeclaration.addClassDeclaration(methodClassDeclaration);
-                        projectDeclarations.add(newProjectDeclaration);
-                    }
-                }
-            }
-        }
-        return projectDeclarations;
-    }
-
-    private static List<Annotation> processServerlessAnnotations(String functionName, ProviderEnum provider) {
-        List<Annotation> annotations = new ArrayList<>();
-        switch (provider) {
-            case AWS, GOOGLE -> {
-                annotations.add(new Annotation("Override", null));
-            }
-            case AZURE -> {
-                Map<String, String> map = new HashMap<>();
-                map.put("name", functionName);
-                annotations.add(new Annotation("FunctionName", map));
-            }
-        }
-        return annotations;
-    }
-
-    private static List<ProjectDeclaration> generateProjectDeclarationMicroservice(ProjectDeclaration projectDeclaration, ProviderEnum provider) {
-        List<ProjectDeclaration> projectDeclarations = new ArrayList<>();
-        return projectDeclarations;
-    }
-
-    public static void generateProjectDeclarationWithFunctionConfig(ProjectDeclaration projectDeclaration) {
-        Infrastructure infrastructure = Main.configuration.getInfrastructure();
-        ProviderEnum provider = Main.configuration.getProvider();
-
-
-    }
 
     public static List<ProjectDeclaration> generateProjectDeclaration(ProjectDeclaration project, Configuration configuration) {
         List<ProjectDeclaration> generatedProjects = new ArrayList<>();

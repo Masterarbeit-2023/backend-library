97105bafa5ebf1ccb45d041b808e2e073b28c1aa
Niklas Röske <n.roeske@ostfalia.de>
2023-11-16 16:03:13 +0100

    create abstract ProjectFileGenerator

---
+++ src/main/java/io/github/masterarbeit/generator/files/ServerlessProjectFilesGenerator.java
@@ -0,0 +1,12 @@
+package io.github.masterarbeit.generator.files;
+
+import io.github.masterarbeit.generator.config.Configuration;
+import io.github.masterarbeit.generator.helper.ProjectDeclaration;
+
+import java.util.List;
+
+public class ServerlessProjectFilesGenerator extends ProjectFileGenerator {
+
+    public void generate(List<ProjectDeclaration> projects, Configuration configuration) {
+    }
+}
